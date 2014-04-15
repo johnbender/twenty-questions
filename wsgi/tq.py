2e39882c2c37cadf03ba990bd4a3253bdb8fd55a
John Bender <john.m.bender@gmail.com>
2014-04-15 15:38:21 -0700

    project skeleton

--- wsgi/tq.py
+++ wsgi/tq.py
@@ -1,11 +1,16 @@
+import os
 from werkzeug.wrappers import Request, Response
+from werkzeug.wsgi import SharedDataMiddleware
 
-def application(environ, start_response):
+def app(environ, start_response):
     request = Request(environ)
-    text = 'Hello %s!' % request.args.get('name', 'World')
-    response = Response(text, mimetype='text/plain')
+    response = Response("Four oh Four", status=404, mimetype='text/plain')
     return response(environ, start_response)
+
+app = SharedDataMiddleware(app, {
+    '/': os.path.join(os.path.dirname(__file__), 'static/')
+})
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('127.0.0.1', 5000, application, use_debugger=True, use_reloader=True)
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
