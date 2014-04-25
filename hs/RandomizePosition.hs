e5473737e6453b667dc4a60c83e4d003b30faf37
John Bender <john.m.bender@gmail.com>
2014-04-25 13:48:49 -0700

    compose

--- hs/RandomizePosition.hs
+++ hs/RandomizePosition.hs
@@ -1,14 +1,12 @@
 import Test.QuickCheck.Arbitrary
 import Test.QuickCheck.Gen
 import Data.Tuple
+import Control.Monad
 
 randomizeFst :: (Gen a, b) -> IO [(a, b)]
 randomizeFst (a, x) = do
   as <- sample' a
   return $ zip as $ repeat x
 
-
 randomizeSnd :: (a, Gen b) -> IO [(a, b)]
-randomizeSnd pair = do
-  as <- randomizeFst $ swap pair
-  return $ map swap as
+randomizeSnd = (return . map swap) <=< (randomizeFst . swap)
